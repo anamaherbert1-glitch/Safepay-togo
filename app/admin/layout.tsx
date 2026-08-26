2037dc8c4d5801ae27a852eddaebf76871ffbae7
anamaherbert1-glitch <anamaherbert1@gmail.com>
2026-08-26 15:47:38 +0000

    Remove legacy admin control-center guard

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,11 +1,5 @@
 import type { ReactNode } from "react";
-import AdminFooterActionGuard from "./AdminFooterActionGuard";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      <AdminFooterActionGuard />
-      {children}
-    </>
-  );
+  return children;
 }
