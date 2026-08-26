549f2bab2c8c848f015e64b68e3182c2a755ff5f
anamaherbert1-glitch <anamaherbert1@gmail.com>
2026-08-26 09:38:14 +0000

    refactor admin navigation: remove floating control center

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,13 +1,11 @@
 import type { ReactNode } from "react";
 import AdminFooterActionGuard from "./AdminFooterActionGuard";
-import AdminControlCenter from "./AdminControlCenter";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <AdminFooterActionGuard />
       {children}
-      <AdminControlCenter />
     </>
   );
 }
