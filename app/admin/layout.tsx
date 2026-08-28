b08c868b79618e2789f7d07f5fd8792482897028
anamaherbert1-glitch <anamaherbert1@gmail.com>
2026-08-28 10:37:28 +0000

    Wire SafePay admin visual dashboard into layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,6 +1,10 @@
 import type { ReactNode } from "react";
 import "./admin-polish.css";
+import AdminVisualLayer from "./AdminVisualLayer";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
-  return children;
+  return <>
+    {children}
+    <AdminVisualLayer />
+  </>;
 }
