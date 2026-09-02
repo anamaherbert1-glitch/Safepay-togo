ef2e19350037b283494defc813c1be71212cd107
anamaherbert1-glitch <anamaherbert1@gmail.com>
2026-09-02 17:14:53 +0000

    feat(admin): connect overview analytics to Supabase

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,12 +2,14 @@
 
 import AdminDashboardLegacy from "./AdminDashboardLegacy";
 import FinanceNavigation from "./FinanceNavigation";
+import OverviewAnalyticsOverlay from "./OverviewAnalyticsOverlay";
 
 export default function AdminPage() {
   return (
     <>
       <AdminDashboardLegacy />
       <FinanceNavigation />
+      <OverviewAnalyticsOverlay />
     </>
   );
 }
