d8a3a4c945ffcaa11a400f887dce43f23af85571
anamaherbert1-glitch <anamaherbert1@gmail.com>
2026-09-02 17:38:20 +0000

    simplify admin page to unified dashboard

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,15 +1,7 @@
 "use client";
 
 import AdminDashboardLegacy from "./AdminDashboardLegacy";
-import FinanceNavigation from "./FinanceNavigation";
-import OverviewAnalyticsOverlay from "./OverviewAnalyticsOverlay";
 
 export default function AdminPage() {
-  return (
-    <>
-      <AdminDashboardLegacy />
-      <FinanceNavigation />
-      <OverviewAnalyticsOverlay />
-    </>
-  );
+  return <AdminDashboardLegacy />;
 }
