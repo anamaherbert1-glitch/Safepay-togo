410940ec1abc633b952541e9628b25ead947871e
anamaherbert1-glitch <anamaherbert1@gmail.com>
2026-09-03 09:45:40 +0000

    connect admin control enhancements to dashboard

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,8 @@
 "use client";
-
 import AdminDashboardLegacy from "./AdminDashboardLegacy";
 import AdminAddOverlay from "./AdminAddOverlay";
+import AdminControlEnhancements from "./AdminControlEnhancements";
 
 export default function AdminPage() {
-  return <><AdminDashboardLegacy /><AdminAddOverlay /></>;
+  return <><AdminDashboardLegacy /><AdminAddOverlay /><AdminControlEnhancements /></>;
 }
