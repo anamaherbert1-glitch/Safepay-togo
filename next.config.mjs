12b46c9d453ed3d9c5aa84c4217a2a9238175fd5
anamaherbert1-glitch <anamaherbert1@gmail.com>
2026-09-12 19:06:38 +0000

    Fix next.config headers syntax

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
       {
         source: "/sw.js",
         headers: [
-          { key: "Cache-Control", "value": "public, max-age=0, must-revalidate" },
+          { key: "Cache-Control", value: "public, max-age=0, must-revalidate" },
           { key: "Service-Worker-Allowed", value: "/" },
         ],
       },
